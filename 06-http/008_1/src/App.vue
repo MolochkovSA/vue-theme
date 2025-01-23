eb3862d86c34cf87844688b6c81b8c5341cbd659
MolochkovSA <wwwsergwww@mail.ru>
2025-01-23 20:52:55 +0300

    Блок 7. Работа с http и сервером

--- 06-http/008_1/src/App.vue
+++ 06-http/008_1/src/App.vue
@@ -1,16 +1,114 @@
 <template>
   <div class="container">
-    <div class="card">
+    <app-alert :alert="alert" @close="alert = null"></app-alert>
+
+    <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данных</h2>
-    </div>
+
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name" />
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+
+    <app-loader v-if="loading"></app-loader>
+
+    <app-people-list v-else :people="people" @load="loadPeople" @remove="removePerson"></app-people-list>
   </div>
 </template>
 
 <script>
+import AppPeopleList from './AppPeopleList'
+import AppAlert from './AppAlert'
+import AppLoader from './AppLoader'
+
 export default {
+  data() {
+    return {
+      name: '',
+      people: [],
+      alert: null,
+      loading: false,
+    }
+  },
+  mounted() {
+    this.loadPeople()
+  },
+  methods: {
+    async createPerson() {
+      // https://vue-with-http-94fbc-default-rtdb.firebaseio.com/people.json
+
+      const responce = await fetch('https://vue-with-http-94fbc-default-rtdb.firebaseio.com/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstName: this.name,
+        }),
+      })
+
+      const firebaseData = await responce.json()
+
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name,
+      })
+
+      this.alert = {
+        type: 'primary',
+        title: 'Успешно!',
+        text: `Пользователь с именем "${this.name}" был создан`,
+      }
+
+      this.name = ''
+    },
+    async loadPeople() {
+      try {
+        this.loading = true
+        const res = await fetch('https://vue-with-http-94fbc-default-rtdb.firebaseio.com/people.json')
+        const data = await new Promise((resolve) => {
+          setTimeout(() => resolve(res.json()), 1500)
+        })
+
+        if (!data) {
+          throw new Error('Список людей пуст')
+        }
+
+        this.people = Object.keys(data).map((key) => ({
+          id: key,
+          ...data[key],
+        }))
+      } catch (error) {
+        this.alert = {
+          type: 'danger',
+          title: 'Ошибка!',
+          text: error.message,
+        }
+      } finally {
+        this.loading = false
+      }
+    },
+    async removePerson(id) {
+      try {
+        await fetch(`https://vue-with-http-94fbc-default-rtdb.firebaseio.com/people/${id}.json`, {
+          method: 'DELETE',
+        })
+        const person = this.people.find((person) => person.id === id)
+        this.people = this.people.filter((person) => person.id !== id)
+
+        this.alert = {
+          type: 'primary',
+          title: 'Успешно!',
+          text: `Пользователь с именем "${person.firstName}" был удален`,
+        }
+      } catch (error) {}
+    },
+  },
+  components: { AppPeopleList, AppAlert, AppLoader },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ 06-http/008_1/src/AppPeopleList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul class="card" v-if="people.length !== 0">
+      <li class="inline" v-for="person in people" :key="person.id">
+        <h3>{{ person.firstName }}</h3>
+        <button class="btn danger" @click="$emit('remove', person.id)">Remove</button>
+      </li>
+    </ul>
+
+    <div class="card center" v-else>
+      <h4>Людей пока нет</h4>
+      <button class="btn" @click="$emit('load')">Загрузить список</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['load', 'remove'],
+  props: ['people'],
+}
+</script>
+
+<style scoped>
+.inline {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
