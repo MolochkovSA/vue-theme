6de689d7163a1d54c7f5412e177d444aff55f060
MolochkovSA <wwwsergwww@mail.ru>
2025-01-24 18:03:40 +0300

    Блок 8. Плагины

--- 07-directives/009_1/src/App.vue
+++ 07-directives/009_1/src/App.vue
@@ -1,41 +1,26 @@
 <template>
   <div class="container">
     <div class="card">
-      <h2 v-color:[type].blink.hover="myColor">Про директивы</h2>
+      <h2>{{ $i18n('app.title') }}</h2>
 
-      <div class="form-control">
-        <label for="inp">Активный по умолчанию</label>
-        <input type="text" id="inp" v-focus />
-      </div>
-
-      <button class="btn" @click="myColor = 'blue'">Сделать синим!</button>
-      <button class="btn" @click="type = type === 'color' ? 'backgroundColor' : 'color'">Переключить тип</button>
+      <button class="btn" @click="changeLang">{{ $i18n('app.changeBtn') }}</button>
     </div>
   </div>
 </template>
 
 <script>
-import { colorDirective } from './colorDirective'
-import { focusDirective } from './focusDirective'
-
 export default {
-  name: 'App',
+  inject: ['changeI18N'],
   data() {
     return {
-      myColor: 'red',
-      type: 'color',
+      modal: false,
     }
   },
-  mounted() {
-    setTimeout(() => {
-      console.log('myColor', this.myColor)
-
-      this.myColor = 'green'
-    }, 3000)
-  },
-  directives: {
-    focus: focusDirective,
-    color: colorDirective,
+  methods: {
+    changeLang() {
+      this.changeI18N('en')
+      this.$forceUpdate()
+    },
   },
 }
 </script>
